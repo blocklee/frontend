4ddc9abcd78e9b1fc192dd8dbddf16fa1e98e53c
blocklee <block.lee121@gmail.com>
2026-03-02 13:46:12 +0800

    fix bug: NEXT_PUBLIC_AD_CUSTOM_CONFIG_URL

--- configs/app/features/adsBanner.ts
+++ configs/app/features/adsBanner.ts
@@ -3,7 +3,7 @@ import type { AdButlerConfig } from 'types/client/adButlerConfig';
 import { SUPPORTED_AD_BANNER_PROVIDERS } from 'types/client/adProviders';
 import type { AdBannerProviders, AdBannerAdditionalProviders } from 'types/client/adProviders';
 
-import { getEnvValue, parseEnvJson } from '../utils';
+import { getEnvValue, getExternalAssetFilePath, parseEnvJson } from '../utils';
 
 const provider: AdBannerProviders = (() => {
   const envValue = getEnvValue('NEXT_PUBLIC_AD_BANNER_PROVIDER') as AdBannerProviders;
@@ -16,7 +16,7 @@ const additionalProvider = getEnvValue('NEXT_PUBLIC_AD_BANNER_ADDITIONAL_PROVIDE
 const title = 'Banner ads';
 
 type AdsBannerFeaturePayload = {
-  provider: Exclude<AdBannerProviders, 'adbutler' | 'none' | 'custom'>;
+  provider: Exclude<AdBannerProviders, 'adbutler' | 'custom' | 'none'>;
 } | {
   provider: 'adbutler';
   adButler: {
@@ -42,7 +42,7 @@ type AdsBannerFeaturePayload = {
 const config: Feature<AdsBannerFeaturePayload> = (() => {
   // 处理meer自定义轮播广告
   if (provider === 'custom') {
-    const configUrl = getEnvValue('NEXT_PUBLIC_AD_CUSTOM_CONFIG_URL');
+    const configUrl = getExternalAssetFilePath('NEXT_PUBLIC_AD_CUSTOM_CONFIG_URL');
     if (configUrl) {
       return Object.freeze({
         title,
